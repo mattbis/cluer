8593ecfcf60f4ea183955de2bc950be187fb27ea
mattbis <mattbis@users.noreply.github.com>
2024-01-08 13:06:19 +0000

    wip: wip

--- src/ts-cluer-v1.ts
+++ src/ts-cluer-v1.ts
@@ -1,7 +1,9 @@
-import {isPrimary,isWorker} from "node:cluster"
-import * as nodePath from "node:path"
-import fs from "node:fs"
-import {EventEmitter} from "node:events"
+import isPrimary from 'node:cluster'
+import isWorker from 'node:cluster'
+
+import {EventEmitter} from 'node:events'
+import fs from 'node:fs'
+import * as nodePath from 'node:path'
 
 namespace cluer {
     // constructor options
@@ -18,13 +20,13 @@ namespace cluer {
     }
     // specifies an index type
     interface IMessage {
-        o: object,
-        t: Date,
-        payload?: <T>
+        // o: object,
+        // t: Date,
+        // payload?: <T>
     }
     interface ICluerNode {
         hash?: string,
-        subver?: number,
+        subversion?: 1 | 2 | 3,
         d: {}
     }
     interface IFlagsOpts {
@@ -42,38 +44,43 @@ namespace cluer {
     }
     export class Cluer {
         static EMPTY_MSG= { o:{}, T:Date.now() }
+        // TODO: metadata about the things
         static OPS: IOperations = {
+            // ---- lifecycle grouping
             // -- worker GoodToGo
             heartbeat() {},
-            // -- index operations
-            create_index(specifier: IIndexSpecifier) {
 
-            },
-            path_exists(specifier: INodeSpecifier) {
+            // -- index grouping
+            create_index(specifier: IIndexSpecifier) {},
+            path_exists(specifier: INodeSpecifier) {},
+            get_index(specifier: IIndexSpecifier) {},
 
-            },
-            get_index(specifier: IIndexSpecifier) {
-            },
+            // ---- node grouping
             // -- hash operations
             get_hash(specifier: INodeSpecifier) {
+
             },
+
             // -- disk operations
             // ==> returns ICluerNode if one or a ICluerRecord if many
-            get_path() {
-            },
+            get_path() {},
+
             // ==> return the number of dirs for a node on FS
             get_node_count(specifier: INodeSpecifier) {
             }
         }
-        static CONFIG= {
-
-        }
+        static CONFIG= {}
         static defaultFlags:IFlagsOpts= {
             isPrimary: false,
             isWorker: false
         }
         f:IFlagsOpts= Cluer.defaultFlags
-        #reset_flags() { this.f=Object.assign({}, Cluer.defaultFlags) }
+        #reset_flags() {
+            this.f=Object.assign({}, Cluer.defaultFlags)
+        }
+        #create_worker() {
+
+        }
         constructor(opts: IClassOptions= {hierarchy: 'primary'}) {
             this.#reset_flags()
             this.f.isPrimary= isPrimary && 'primary'===opts.hierarchy
@@ -84,20 +91,44 @@ namespace cluer {
                 const {func, payload}= op
                 let newMessage= Object.assign({}, Cluer.EMPTY_MSG)
                 Cluer.OPS[func](payload)
-
             }
         }
         listen_msg(message: IMessage) {
             if (isWorker) {
-
+                try {}
+                catch(err) {
+                    // trigger flag
+                }
             }
         }
     }
-    /* extend the class with a Fastify server */
+
+    /* extend the class with a FastifyServer - to serve a control panel */
     export class FastifyServer extends Cluer {
         constructor() {
             super()
         }
-
+        // view() {},
+        // properties() {}
+        static OPS= {
+            get_index() {}
+        }
     }
+
+    /* self contained HTML Interface */
+    export class HTMLControlPanel {
+        constructor() {
+            // vue js --
+            
+        }
+        /* */
+        #setup(){}
+        static OPS= {
+            // get index component to inject control panel
+            get_index() {},
+            get_control_panel() {}
+        }
+    }
+
 }
+
